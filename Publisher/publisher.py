82815b5ee80ba5981d6cebd6b873e77437a0b62a
manoj4617 <manojsainikwod4617@gmail.com>
2023-09-12 20:10:39 +0530

    refactored publisher and subscriber

--- Publisher/publisher.py
+++ Publisher/publisher.py
@@ -1,61 +1,84 @@
+from datetime import datetime
 import paho.mqtt.client as mqtt
 import random
 import time
 import json
-from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class SensorPublisher:
+    def __init__(self, broker_addr, port, topics) -> None:
+        self.broker_addr        = broker_addr
+        self.port               = port
+        self.topics             = topics
+        self.client             = mqtt.Client("Publisher")
+
+        # Set the callbacks, this function will be called when the message is published
+        self.client.on_publish  = self.on_publish
+        self.client.on_connect  = self.on_connect
+
+    def on_publish(self, client, userdata, mid):
+        logger.info(f"Message published with MID {mid}")
+    
+    def on_connect(self, client, userdata, flags, return_code):
+        if return_code == 0:
+            logger.info("Connected")
+        else:
+            logger.error(f"Coulld not connect, return code: {return_code}")
+
+    def publish_message(self,topic_name, message):
+        try:
+            (result,mid) = self.client.publish(topic_name, json.dumps(message), qos=1)
+            if result != mqtt.MQTT_ERR_SUCCESS:
+                logger.info(f"Failed to publish data to topic {topic_name}")
+            return (result, mid)
+        except Exception as e:
+            logger.error(f"Error publish message: {str(e)}")
+
+    def generate_sensor_data(self, sensor_type):
+        if sensor_type == "temperature":
+            return {
+                "sensor_id" : "sensor_temperature_" + str(round(random.uniform(10,20))),
+                "value" :  round(random.uniform(20,30),4),
+                "timestamp" : datetime.now().isoformat()
+            }
+        elif sensor_type == "humidity":
+            return {
+                "sensor_id" : "sensor_humidity_" + str(round(random.uniform(10,20))),
+                "value" :  round(random.uniform(40, 60), 4),
+                "timestamp" : datetime.now().isoformat()
+            }
+        else:
+            return None
+
+    def run(self):
+        self.client.connect(self.broker_addr, self.port)
+        self.client.loop_start()
+
+        try:
+            while True:
+                for topic in topics:
+                    sensor_data  = self.generate_sensor_data(topic.split("/")[-1])
+                    if sensor_data:
+                        self.publish_message(topic, sensor_data)
+
+                time.sleep(5)
+        finally:
+            self.client.loop_stop()
+
+        
 
 
-broker_addr         = "mosquitto"
-port                = 1883
+if __name__ == "__main__":
+    broker_addr         = "mosquitto"
+    port                = 1883
 
-temperature_topic   = "sensor/temperature"
-humidity_topic      = "sensor/humidity"
+    topics   = ["sensor/temperature","sensor/humidity"]
 
-client = mqtt.Client("Publisher")
-
-def on_publish(client, userdata, mid):
-    print(f"Message published with MID {mid}")
+    publisher           = SensorPublisher(broker_addr, port, topics)
+    publisher.run()
 
 
-def on_connect(client, userdata, flags, return_code):
-    if return_code == 0:
-        print("Connected")
-    else:
-        print(f"Coulld not connect, return code: {return_code}")
-
-# Set the callbacks, this function will be called when the message is published
-client.on_publish = on_publish
-client.on_connect = on_connect
-
-def publish_message(topic_name, message):
-    (result,mid) = client.publish(topic_name, json.dumps(message), qos=1)
-    return (result, mid)
-
-client.connect(broker_addr, port)
-client.loop_start()
-
-try:
-    while True:
-        sensor_data = {
-            "sensor_id" : "sensor_temperature_" + str(round(random.uniform(10,20))),
-            "value" :  round(random.uniform(20,30),4),
-            "timestamp" : datetime.now().isoformat()
-        }
-
-        #publish temperature data
-        (result,mid) = publish_message(temperature_topic, sensor_data)
-        if result != mqtt.MQTT_ERR_SUCCESS:
-            print(f"Failed to publish temperature data with MID: {mid}")
-
-        # modify message for humidity sensors
-        sensor_data["sensor_id"] = "sensor_humidity_" + str(round(random.uniform(10,20)))
-        sensor_data["value"] = round(random.uniform(40, 60), 4)
-
-        (result,mid) = publish_message(humidity_topic, sensor_data)
-        if result != mqtt.MQTT_ERR_SUCCESS:
-            print(f"Failed to publish humidity data with MID: {mid}")
-
-        time.sleep(5)
-finally:
-    client.loop_stop()
 
--- Subscriber/subscriber.py
+++ Subscriber/subscriber.py
@@ -2,75 +2,95 @@ import paho.mqtt.client as mqtt
 from pymongo import MongoClient
 import json
 import redis
+import logging
 
-broker_addr         = "mosquitto"
-port                = 1883
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-temperature_topic   = "sensor/temperature"
-humidity_topic      = "sensor/humidity"
+class SensorSubscriber:
+    def __init__(self, mongo_uri, redis_host, redis_port):
+        self.mongo_uri                  = mongo_uri
+        self.redis_host                 = redis_host
+        self.redis_port                 = redis_port
+        
+        self.broker_addr                = "mosquitto"
+        self.port                       = 1883
+        self.temperature_topic          = "sensor/temperature"
+        self.humidity_topic             = "sensor/humidity"
 
-mongo_uri           = "mongodb://mqtt-mongodb:27017"
-mongo_client        = MongoClient(mongo_uri)
-db                  = mongo_client["sensor_data"]
+        self.mqtt_client                = mqtt.Client("Subscriber")
+        self.mqtt_client.on_connect     = self.on_connect
+        self.mqtt_client.on_message     = self.on_message
 
-redis_host          = "redis-mqtt"
-redis_port          = 6379
-redis_client        = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+        self.mongo_client               = MongoClient(self.mongo_uri)
+        self.db                         = self.mongo_client["sensor_data"]
 
-def save_latest_ten_messages(sensor_id,value,timestamp, topic_name):
-
-    reading = {
-        "sensor_id" : sensor_id,
-        "value" :value,
-        "timestamp" : timestamp
-    }
-
-    # convert the dict to json string
-    json_reading = json.dumps(reading)
-
-    # inserts message to the front of the list for a topic
-    redis_list_key = f"{topic_name}_sensor_readings"
-    redis_client.lpush(redis_list_key, json_reading)
-
-    # when a new message is inserted using the above lpush method
-    # it is inserted at the begining and it is trimmed using
-    # the below function call, only the first 10 messages
-    # are saved 10th message is discarded
-    redis_client.ltrim(redis_list_key, 0, 9)
-
-def save_to_db(message):
-
-    payload         = json.loads(message.payload)
-    collection_name = "temperature" if message.topic == temperature_topic else "humidity"
-    collection      = db[collection_name]
-
-    # save to database
-    collection.insert_one(payload)
-    print(f"Saved {collection_name} data to Database. Payload: {payload}")
-
-    # save into in-memory-database
-    save_latest_ten_messages(payload["sensor_id"], payload["value"], payload["timestamp"], collection_name)
-    print(f"Saved Payload: {payload} to redis")
+        self.redis_client               = redis.StrictRedis(host=self.redis_host, port=self.redis_port, decode_responses=True)
     
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print(f"Connected to MQTT broker with code {rc}")
-        client.subscribe([(temperature_topic, 1), (humidity_topic, 1)])
-    else:
-        print(f"could not connect, return code: {rc}")
+    def connect(self):
+        self.mqtt_client.connect(self.broker_addr, self.port)
+        self.mqtt_client.loop_forever()
 
-def on_message(client, userdata, message):
-    try:
-        save_to_db(message)
-    except Exception as exp:
-        print(f"Error saving data to Database: {exp}")
-    
+    def save_latest_ten_messages(self, sensor_id, value, timestamp, topic_name):
+        reading = {
+            "sensor_id": sensor_id,
+            "value": value,
+            "timestamp": timestamp,
+        }
+        
+        # convert the dict to json string
+        json_reading = json.dumps(reading)
 
-client = mqtt.Client("Subscriber")
+        # inserts message to the front of the list for a topic
+        redis_list_key = f"{topic_name}_sensor_readings"
+        self.redis_client.lpush(redis_list_key, json_reading)
 
-client.on_connect = on_connect
-client.on_message = on_message
+        # when a new message is inserted using the above lpush method
+        # it is inserted at the begining and it is trimmed using
+        # the below function call, only the first 10 messages
+        # are saved 10th message is discarded
+        self.redis_client.ltrim(redis_list_key, 0, 9)
 
-client.connect(broker_addr, port)
+    def save_to_db(self, message):
+        payload = json.loads(message.payload)
 
-client.loop_forever()
+        if message.topic.startswith(self.temperature_topic):
+            collection_name = "temperature"
+        elif message.topic.startswith(self.humidity_topic):
+            collection_name = "humidity"
+        else:
+            return
+
+        collection = self.db[collection_name]
+        collection.insert_one(payload)
+
+        logger.info(f"Saved {collection_name} to database payload: {payload}")
+        try:
+            self.save_latest_ten_messages(
+                payload["sensor_id"], payload["value"], payload["timestamp"], collection_name
+            )
+            logger.info(f"Saved payload to redis")
+        except Exception as exp:
+            logger.error(f"Error saving data to redis: {exp}")
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            logger.info(f"Connected to MQTT broker with code {rc}")
+            client.subscribe([(self.temperature_topic, 1), (self.humidity_topic, 1)])
+        else:
+            logger.error(f"Could not connect, return code: {rc}")
+
+    def on_message(self, client, userdata, message):
+        try:
+            self.save_to_db(message)
+        except Exception as exp:
+            logger.error(f"Error saving data to Database: {exp}")
+
+
+if __name__ == "__main__":
+    subscriber = SensorSubscriber(
+        mongo_uri   = "mongodb://mqtt-mongodb:27017",
+        redis_host  = "redis-mqtt",
+        redis_port  = 6379,
+    )
+    subscriber.connect()
